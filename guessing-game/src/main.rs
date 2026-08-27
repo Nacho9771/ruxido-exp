e45968df3732ffb9464f5207cac4006da98781f5
Connor Gordon <cgordon0@byu.edu>
2026-08-27 10:16:49 -0600

    I think i'm committing data locations

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -4,25 +4,29 @@ use std::cmp::Ordering;
 
 
 fn main() {
-    loop {
-        println!("Guess the number!");
+    let mut guessed_once = true;
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("Guess a number between 1 and 1,000,000!");
 
-        println!("Please input a number between 1 and one million");
+    loop {
         let mut guess = String::new();
-        let secret_number = rand::thread_rng().gen_range(1..=100);
-        let guess: u32 = guess.trim().parse().expect();
 
         let bytes_read = io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-        if bytes_read > 0 & guessed_once = true {
-            println!("Just kidding haha, please guess a number from 1-100");
+        if bytes_read > 0 && guessed_once {
+            println!("Just kidding haha, please guess a number between 1 and 100.");
+            guessed_once = false;
         };
 
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too small! Guess again."),
+            Ordering::Greater => println!("Too big! Guess again."),
             Ordering::Equal => {
                 println!("You win!");
                 break;
