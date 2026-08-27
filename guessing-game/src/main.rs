15e1834f145f21af507cd19882843a84d3b61162
Connor Gordon <cgordon0@byu.edu>
2026-08-27 09:57:00 -0600

    Complete chapters 1-3 of the book

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -4,23 +4,29 @@ use std::cmp::Ordering;
 
 
 fn main() {
-    println!("Guess the number!");
+    loop {
+        println!("Guess the number!");
 
-    println!("Please input a number between 1 and one million");
-    let mut guess = String::new();
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+        println!("Please input a number between 1 and one million");
+        let mut guess = String::new();
+        let secret_number = rand::thread_rng().gen_range(1..=100);
+        let guess: u32 = guess.trim().parse().expect();
 
-    let bytes_read = io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let bytes_read = io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    if bytes_read > 0 {
-        println!("Just kidding haha, please guess a number from 1-100");
-    };
+        if bytes_read > 0 & guessed_once = true {
+            println!("Just kidding haha, please guess a number from 1-100");
+        };
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
     }
 }
