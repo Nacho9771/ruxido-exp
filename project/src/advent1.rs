9a8329c7e27dd9f229dfd214e1aebf7d630d6929
Connor Gordon <cgordon0@byu.edu>
2026-08-31 16:48:15 -0600

    finish advent day 1 (arrange code so that it handles overflow up to 99)

--- project/src/advent1.rs
+++ project/src/advent1.rs
@@ -1,34 +1,16 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-fn find_direction(direction: &str, number: u64, mut password: u64) -> u64 {
+fn find_direction(direction: &str, number: u64, password: u64) -> u64 {
     if direction == "R" {
-        password = add_direction(number, password);
+        (password + 100 - (number % 100)) % 100
     } else {
-        password = subtract_direction(number, password);
+        (password + number) % 100
     }
-
-    password
-}
-
-fn subtract_direction(number: u64, password: u64) -> u64 {
-    if number > 99 {
-        subtract_direction( number - 99, password);
-    };
-    println!("{password}");
-    password - number
-}
-
-fn add_direction(number: u64, password: u64) -> u64 {
-    if number > 99 {
-        add_direction(number - 99, password);
-    };
-    println!("{password}");
-    password + number
 }
 
 fn find_password<T: BufRead>(reader: T) -> u64 {
-    let password: u64 = 50;
+    let mut password: u64 = 50;
     let base: u64 = 0;
     let mut answer: u64 = 0;
 
@@ -37,16 +19,17 @@ fn find_password<T: BufRead>(reader: T) -> u64 {
 
         let direction = &line[0..1];
         let number: u64 = match &line[1..].trim().parse() {
-        Ok(num) => *num,
-        Err(_) => continue,
+            Ok(num) => *num,
+            Err(_) => continue,
         };
 
-        let password = find_direction(&direction, number, password);
+        password = find_direction(direction, number, password);
         if password == base {
             answer += 1;
         }
     }
 
+    println!("{answer}");
     answer
 }
 
@@ -54,5 +37,5 @@ fn main() {
     let file = File::open("input").expect("Failed to Read File.");
     let reader = io::BufReader::new(file);
 
-    let password = find_password(reader);
+    find_password(reader);
 }
